49afe1e6883ff70ab5f4ac980f608173eaf5a29b
duytdph21921 <101155843+duytdph21921@users.noreply.github.com>
2022-11-24 09:21:20 +0700

    Add project files.

---
+++ A_DAL/Repositories/Anhs.cs
@@ -0,0 +1,86 @@
+﻿using A_DAL.DomaninClass;
+using A_DAL.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace A_DAL.Repositories
+{
+    public class Anhs : IAnh
+    {
+        public XeMayDbConText _dbcontext = new XeMayDbConText();
+        public bool add(Anh anh)
+        {
+            try
+            {
+                if (anh == null) return false;
+                
+                _dbcontext.Anhs.Add(anh);
+                _dbcontext.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+               //
+                return false;
+            }
+          
+        }
+
+        public bool delete(Anh anh)
+        {
+            try
+            {
+                if (anh.id == null) return false;
+                {
+                    //  var getbyIdAnh = _dbcontext.Anhs.FirstOrDefault(c => c.id == anh.id); 
+                    // có thể sửa dụng cách này
+                    _dbcontext.Anhs.Remove(_dbcontext.Anhs.FirstOrDefault(c => c.id == anh.id));
+                    _dbcontext.SaveChanges();
+                    return true;
+                }
+              
+            }
+            catch (Exception e)
+            {
+                
+                return false;
+                
+            }
+          
+
+        }
+
+        public List<Anh> GetAll()
+        {
+            var getall = _dbcontext.Anhs.ToList();
+            return getall;
+        }
+
+        public bool update(Anh anh)
+        {
+            try
+            {
+                if (anh.id == null) return false;
+                {
+                    //  var getbyIdAnh = _dbcontext.Anhs.FirstOrDefault(c => c.id == anh.id); 
+                    // có thể sửa dụng cách này
+                    _dbcontext.Anhs.Update(_dbcontext.Anhs.FirstOrDefault(c => c.id == anh.id));
+                    _dbcontext.SaveChanges();
+                    return true;
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                return false;
+
+            }
+
+        }
+    }
+}
